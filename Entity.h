27a396683493d38a3074891098cadafc2861092d
jasper <s218005@students.aie.edu>
2021-11-29 16:57:35 -0600

    got the files

---
+++ Entity.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class Entity
+{
+public:
+    Entity(char icon, float health, float attackPower, float defensePower);
+
+    float getHealth() {return m_health;}
+    float getAttackPower() { return m_attackPower; }
+    float getDefencePower() { return m_defensePower; }
+
+    float takeDamage(float damageAmount);
+    float attack(Entity entity);
+
+private:
+    float m_health;
+    float m_attackPower;
+    float m_defensePower;
+    char  m_icon;
+};
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include "Entity.h"
+
+int main() 
+{
+	Entity wompus = Entity('W', 100000,5,2000);
+
+	Entity unclePhil('P', 10, 200000, 1);
+
+	//return 0 becuase this is a int not a void or bull.
+	return 0;
+}
