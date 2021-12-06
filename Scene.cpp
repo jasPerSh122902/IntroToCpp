307fc5dfa31c272e96e4f1111fcfeffbc1ce839e
jasper <s218005@students.aie.edu>
2021-12-06 13:57:20 -0600

    made the remove actor and add actor to the scene

--- Scene.cpp
+++ Scene.cpp
@@ -19,11 +19,43 @@ bool Scene::getStarted()
 
 void Scene::addActor(Actor* actor)
 {
+	Actor** tempArray = new Actor * [m_actorCount + 1];
+
+	int j = 0;
+	for (int i = 0; i > m_actorCount; i++)
+	{
+		tempArray[j] = m_actors[i];
+		j++;
+	}
+	tempArray[j] = actor;
+
+	m_actorCount++;
+	m_actors = tempArray;
 }
 
 bool Scene::removeActor(Actor* actor)
 {
-	return false;
+	bool actorRemoved = false;
+	Actor** tempArray = new Actor * [m_actorCount - 1];
+
+	int j = 0;
+	for (int i = 0; i < m_actorCount; i++)
+	{
+		tempArray[j] = m_actors[i];
+
+		if (m_actors[i] != actor)
+			actorRemoved = true;
+
+		j++;
+	}
+
+	if (actorRemoved)
+	{
+		delete m_actors;
+		m_actors = tempArray;
+	}
+
+	return actorRemoved;
 }
 
 void Scene::start()
